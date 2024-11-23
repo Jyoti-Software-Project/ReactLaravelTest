498c33597b4b904ca43b95b9221bf4651de1b89a
Jyoti-Software-Project <jyotiprakashmal104@gmail.com>
2024-11-23 13:30:00 +0530

    class component and function component of react

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import UsersC from './UsersC';
+import UsersF from './UsersF';
 
 function App() {
   return (
     <div className="App">
       React App
       <h2>Users</h2>
+      {/* Class component is used in UserC and Function component is used in UserF */}
       <UsersC />
+      {/* <UsersF /> */}
     </div>
   );
 }
